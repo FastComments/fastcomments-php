779d0a9bdbbc4eeb79f271f4c13d0a68c1d61cc2
winrid <winrid@gmail.com>
2025-10-30 20:35:13 -0700

    Adding Spec

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 rm -rf lib/Api lib/Model docs/Api docs/Model test/Api test/Model
-wget http://localhost:3001/js/swagger.json -O /tmp/fastcomments-openapi.json
-openapi-generator-cli generate -i /tmp/fastcomments-openapi.json -g php -o . -c config.json
+wget http://localhost:3001/js/swagger.json -O ./openapi.json
+openapi-generator-cli generate -i ./openapi.json -g php -o . -c config.json
 
 # Fix composer.json to ensure it has required fields
 node fix-composer.js
